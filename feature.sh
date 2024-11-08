c29ae1bebf2a793d49dea7fd64bcf0970e63ccc2
abhay <abhay10371@gmail.com>
2024-11-08 14:55:53 +0530

    Added command to check path

--- feature.sh
+++ feature.sh
@@ -14,5 +14,6 @@ echo "Install nginx package: $(sudo apt install nginx -y)"
 echo "Check Installed Packages: $(sudo apt list --installed)"
 echo "Check running processes: $(ps aux)"
 echo "Check RAM usage: $(free -h)"
+echo "Check binary path: ${PATH}"
 echo -e "\033[0;37m"
 
