b140d42450d1e8b6de1647f69ac293b6fc9dc32d
abhay <abhay10371@gmail.com>
2024-11-08 12:52:47 +0530

    Added nginx

--- feature.sh
+++ feature.sh
@@ -10,5 +10,6 @@ echo "Update the system: $(sudo apt update)"
 echo "Upgrade the system: $(sudo apt upgrade -y)"
 echo "Install tree package: $(sudo apt install tree -y)"
 echo "Install htop package: $(sudo apt install htop -y)"
+echo "Install nginx package: $(sudo apt install nginx -y)"
 echo -e "\033[0;37m"
 
