2bb32a397e5f15872e67135c6dd97522667addee
abhay <abhay10371@gmail.com>
2024-11-08 13:00:03 +0530

    Check running process

--- feature.sh
+++ feature.sh
@@ -12,5 +12,6 @@ echo "Install tree package: $(sudo apt install tree -y)"
 echo "Install htop package: $(sudo apt install htop -y)"
 echo "Install nginx package: $(sudo apt install nginx -y)"
 echo "Check Installed Packages: $(sudo apt list --installed)"
+echo "Check running processes: $(ps aux)"
 echo -e "\033[0;37m"
 
